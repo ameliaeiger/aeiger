85f87344dab3e446cfe3b7a953ff2ce44a32a801
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-08-22 12:19:43 -0600

    Fix url check for image display

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react"
 import "./Header.css"
-import headerImage from '../images/plains.png';
 
 
 const Header = () => {
@@ -9,18 +8,18 @@ const Header = () => {
 
     //Get current page url and set state
     useEffect(() => {
-        setCurrentPage(window.location.href.substring(21))
+        setCurrentPage(window.location.href)
         setHeaderImage(getHeaderImage())
     },[currentPage])
 
 
     //Translates current page to readable string for CSS class
     const getHeaderImage = () => {
-        if (currentPage && currentPage=="/") {
+        if (currentPage && currentPage.includes("/") && currentPage.length<30) {
             console.log("home")
             return "home"
         }
-        if (currentPage && currentPage=="/portfolio"){
+        if (currentPage && currentPage.includes("portfolio")){
             console.log("portfolio")
             return "portfolio"
         }
